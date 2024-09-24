6b024a4fdcab4a03883831cf819335f994ac618f
Venky <venkatesh.jakkam@gmail.com>
2024-09-24 15:45:13 +0530

    secondCommit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCmxrOgMTHo4Uud4Ts61IJfARjzAULawy8",
+  authDomain: "crypto-coin-app-12eba.firebaseapp.com",
+  projectId: "crypto-coin-app-12eba",
+  storageBucket: "crypto-coin-app-12eba.appspot.com",
+  messagingSenderId: "1077042052813",
+  appId: "1:1077042052813:web:480e8c4ae0d92833ab7384",
+};
+ 
+export default firebaseConfig;
+  
